6f5ad743c7fcf24f65bae9291cd87503148d2c19
aidenZ1193 <aidenz1193@gmail.com>
2018-10-20 23:35:00 -0700

    Done with tutorial - this is the complete runnable version of pet-shop. Moving to change it to Cars

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -5,34 +5,30 @@ import "truffle/DeployedAddresses.sol";  // 用来获取被测试合约的地址
 import "../contracts/Adoption.sol";      // 被测试合约
 
 contract TestAdoption {
-  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
   // 领养测试用例
-  function testUserCanAdoptPet() public {
-    uint returnedId = adoption.adopt(8);
+    function testUserCanAdoptPet() public {
+        uint returnedId = adoption.adopt(8);
 
-    uint expected = 8;
-    Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
-  }
+        uint expected = 8;
+        Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+    }
 
   // 宠物所有者测试用例
-  function testGetAdopterAddressByPetId() public {
-    // 期望领养者的地址就是本合约地址，因为交易是由测试合约发起交易，
-    address expected = this;
-    address adopter = adoption.adopters(8);
-    Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
-  }
+    function testGetAdopterAddressByPetId() public {
+        // 期望领养者的地址就是本合约地址，因为交易是由测试合约发起交易，
+        address expected = this;
+        address adopter = adoption.adopters(8);
+        Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+    }
 
     // 测试所有领养者
-  function testGetAdopterAddressByPetIdInArray() public {
-  // 领养者的地址就是本合约地址
-    address expected = this;
-    address[16] memory adopters = adoption.getAdopters();
-    Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
-  }
+    function testGetAdopterAddressByPetIdInArray() public {
+      // 领养者的地址就是本合约地址
+        address expected = this;
+        address[16] memory adopters = adoption.getAdopters();
+        Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
+    }
 }
 
-作者：yuyangray
-链接：https://www.jianshu.com/p/c570045078e6
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
